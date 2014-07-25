f1c29a7d4903f5ca40b20daeb8ae36aa5bfa6c9a
Jaco Joubert <joubert.jaco@gmail.com>
2014-07-25 12:20:28 -0400

    add autoprefixer, go down to 0.37 for ember-cli, add velocity

--- Brocfile.js
+++ Brocfile.js
@@ -1,17 +1,35 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var autoprefixer = require('broccoli-autoprefixer');
+var p = require('ember-cli/lib/preprocessors');
+var preprocessCss = p.preprocessCss;
 
 var app = new EmberApp({
-  autoprefixer: {
-    browsers: ['last 2 ios version']
-  }
+  name: require('./package.json').name,
+
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
+
+  getEnvJSON: require('./config/environment')
 });
 
+app.import('vendor/Velocity.js/jquery.velocity.min.js');
+
+// emberui
 app.import('vendor/emberui/dist/named-amd/emberui.js', {
-    'emberui': ['default']
+  'emberui': ['default']
 });
 app.import('vendor/emberui/dist/emberui.css');
 app.import('vendor/emberui/dist/default-theme.css');
+
+prefixCSS: {
+  options: {
+    browsers: ['last 2 versions']
+  }
+}
+
 
 module.exports = app.toTree();
